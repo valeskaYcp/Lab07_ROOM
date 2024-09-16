f3d1014015c48cad671bca0366c38207d4c26583
valeskaYcp <valeska.condori@tecsup.edu.pe>
2024-09-16 16:23:30 -0500

    Creacion de la interz UserDao y la clase UserDatabase

---
+++ app/src/main/java/com/example/datossinmvvm/UserDao.kt
@@ -0,0 +1,14 @@
+package com.example.datossinmvvm
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM User")
+    suspend fun getAll(): List<User>
+
+    @Insert
+    suspend fun insert(user: User)
+}
